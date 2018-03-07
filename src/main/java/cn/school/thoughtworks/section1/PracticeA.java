05a3ffe4e59fa5aacdf50fc0d05ac339f75a889e
Pei Zhang <pezhang@thoughtworks.com>
2018-03-07 16:45:15 +0800

    finish 1-2

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-        List<String> result = new ArrayList<>(collection1);
+        List<String> result = new ArrayList<>(collection1); //防止collection1被修改
         result.retainAll(collection2);
         return result;
     }
--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,12 +1,14 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        List<String> sameElements = Arrays.asList("a","e","f","d");
-        return sameElements;
+        List<String> result = new ArrayList<>(collection1); //防止collection1被修改
+        result.retainAll(collection2.get(0));
+        return result;
     }
 }
